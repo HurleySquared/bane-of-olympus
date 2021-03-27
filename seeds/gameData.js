c0d5c4c7f83aa5482dfbbcec224076de56df8364
Shawn Fox <foxeasttrojan06@gmail.com>
2021-03-27 14:32:35 -0400

    char post, will work on the input to this next

--- seeds/gameData.js
+++ seeds/gameData.js
@@ -3,8 +3,8 @@ const { Game } = require("../models");
 const gamedata = [
   {
     level: 1,
-    score: "",
-    character_id: "",
+    score: 0,
+    character_id: 1,
     user_id: 1,
   },
 ];
--- seeds/index.js
+++ seeds/index.js
@@ -1,4 +1,4 @@
-const sequelize = require("../config/connections");
+const sequelize = require("../config/connection");
 const seedCharacters = require("./characterData");
 const seedGame = require("./gameData");
 const seedUser = require("./userData");
@@ -7,11 +7,11 @@ const seedEnemies = require("./enemiesData");
 const seedAll = async () => {
   await sequelize.sync({ force: true });
 
-  await seedCharacters();
-
-  await seedGame();
-
   await seedUser();
+  
+  await seedGame();
+  
+  await seedCharacters();
 
   await seedEnemies();
 
