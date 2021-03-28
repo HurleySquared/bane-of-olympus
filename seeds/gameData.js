2136fbc7a21c7b565d430e1ca14024e5d3ff91a3
Uriel Rosario <urielrosario1010@gmail.com>
2021-03-27 21:02:08 -0400

    fixed seeds

--- seeds/gameData.js
+++ seeds/gameData.js
@@ -7,6 +7,18 @@ const gamedata = [
     character_id: "",
     user_id: 1,
   },
+  {
+    level: 2,
+    score: "",
+    character_id: "",
+    user_id: 2,
+  },
+  {
+    level: 1,
+    score: "",
+    character_id: "",
+    user_id: 3,
+  },
 ];
 
 const seedGame = () => Game.bulkCreate(gamedata);
