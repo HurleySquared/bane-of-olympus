1af6435f03b6d1bb39697495be67479e40be24a4
ian mcbrayer <mcbrayer.ian@gmail.com>
2021-03-29 13:05:40 -0400

    completed adjustments for displaying attack names

--- public/js/battleAnimation.js
+++ public/js/battleAnimation.js
@@ -1,33 +1,9 @@
 const playerAvatar = document.getElementById('player1');
 const enemyAvatar = document.getElementById('enemy');
 const chooseAttack = document.getElementById('choose-attack');
+// const moveAlert = document.getElementById('move-alert');
 const userAlert = document.getElementById('user-alert');
 const enemyAlert = document.getElementById('enemy-alert');
-const charAttack1 = document.getElementById('attack1');
-const charAttack2 = document.getElementById('attack2');
-const charAttack3 = document.getElementById('attack3');
-const characterImage = character.charImage;
-
-const setAttacks = () => {
-  
-  switch (characterImage) {
-    case '/images/mage.png':
-      charAttack1.innerHTML = "Water Jet";
-      charAttack2.innerHTML = "Prayer of Healing";
-      charAttack3.innerHTML = "Fireball";
-      break;
-    case '/images/beast.png':
-      charAttack1.innerHTML = "Slash";
-      charAttack2.innerHTML = "Reckless Swing";
-      charAttack3.innerHTML = "Throw Sand";
-      break;
-    case '/images/hunter.png':
-      charAttack1.innerHTML = "Water Jet";
-      charAttack2.innerHTML = "Prayer of Healing";
-      charAttack3.innerHTML = "Trick Shot";
-      break;
-  };
-};
 
 const battleMove = () => {
   headerVisible();
@@ -77,8 +53,7 @@ const resetEnemyAlert = () => {
 
 const resetButton = () => {
   document.getElementsByTagName('button').disabled = true;
-  setTimeout(function() {document.getElementsByTagName('button').disabled = false}, 7000);
+  setTimeout(function() {document.getElementByTagName('button').disabled = false}, 7000);
 }
 
-// battleMove();
-setAttacks();
+battleMove();
--- public/js/setAttacks.js
+++ public/js/setAttacks.js
@@ -1,11 +1,11 @@
 const charAttack1 = document.getElementById('attack1');
 const charAttack2 = document.getElementById('attack2');
 const charAttack3 = document.getElementById('attack3');
-const characterImage = character.charImage;
+const characterImage = document.getElementById('player-image').getAttribute("src");
 
 const setAttacks = () => {
   
-  switch (charImage) {
+  switch (characterImage) {
     case '/images/mage.png':
       charAttack1.innerHTML = "Water Jet";
       charAttack2.innerHTML = "Prayer of Healing";
