ea98f4cac884b0e71f16d920c6faf1e85d047d62
Uriel Rosario <urielrosario1010@gmail.com>
2021-03-29 10:03:52 -0400

    leaderboard

--- controllers/api/leaderboard-routes.js
+++ controllers/api/leaderboard-routes.js
@@ -1,27 +1,34 @@
 const router = require("express").Router();
 const { User, Characters, Game } = require("../../models");
-
 const withAuth = require("../../utils/auth");
 
-router.get('/', withAuth, async (req, res) => {
-    try {
-        const leaderboardData = await User.findAll({
-           include: [
-               {
-                   model: Game,
-               }
-           ]
-        });
-        const leaderboard = leaderboardData.map((leaderboard) =>
-          leaderboard.get({ plain: true })
-        );
-        res.render('leaderboard', {
-            leaderboard,
-        });
-    }   catch (err) {
-        console.log(err);
-        res.status(500).json(err);
+router.get("/", async (req, res) => {
+  try {
+    const leaderboardData = await User.findAll({
+      include: [
+        {
+          model: Game,
+        },
+      ],
+    });
+    const leaderboard = await JSON.parse(JSON.stringify(leaderboardData));
+    const leaderArray = [];
+    for (const eachUser of leaderboard) {
+      const usersGame = await Game.findOne({
+        where: { user_id: eachUser.id },
+      });
+      leaderArray.push([eachUser.username, usersGame.score]);
     }
+    await leaderArray.sort((a, b) => {
+      return b[1] - a[1];
+    });
+    console.log(leaderArray);
+    res.render("leaderboard", {
+      leaderArray,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
-
 module.exports = router;
